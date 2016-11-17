1e7933d08180c6237f838aecb9b908ce55a73850
Matt Chapman <chapabu101@gmail.com>
2016-11-17 17:48:17 +0000

    Added comments and docblocks, removed underscores..we have no private functions here in JS land.

--- libs/config/configLoader.js
+++ libs/config/configLoader.js
@@ -3,33 +3,63 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 
+/**
+ * Load configuration from files.
+ *
+ * @class
+ */
 function configLoader() {
 }
 
+/**
+ * Load configuration from either a JS or YAML file.
+ *
+ * @throws {Error} Thrown if no YAML or JavaScript config file is found.
+ *
+ * @returns {object} Holograph configuration.
+ */
 configLoader.prototype.load = function () {
 
-  if (this._hasJSConfig()) {
-    return this._loadJsConfig();
+  if (this.hasJSConfig()) {
+    return this.loadJsConfig();
   }
 
-  if (this._hasYAMLConfig()) {
-    return this._loadYamlConfig();
+  if (this.hasYAMLConfig()) {
+    return this.loadYamlConfig();
   }
 
+  // If we've not been able to find a config file, then bomb out.
   throw new Error('No holograph configuration file found.');
 
 };
 
-configLoader.prototype._loadJsConfig = function () {
+/**
+ * Load a JavaScript based configuration file.
+ *
+ * @returns {object} Holograph configuration loaded from a JavaScript file.
+ */
+configLoader.prototype.loadJsConfig = function () {
   return require('./holograph_config');
 };
 
-configLoader.prototype._loadYamlConfig = function () {
+/**
+ * Load a YAML based configuration file.
+ *
+ * @returns {object} Holograph configuration loaded from a YAML file.
+ */
+configLoader.prototype.loadYamlConfig = function () {
+  // We don't really need to wrap this in a try/catch as chekcing for the existence of this file hapens directly prior
+  // to loading it in. Maybe.
   const yamlConfig = fs.readFileSync('holograph_config.yml');
   return yaml.safeLoad(yamlConfig);
 };
 
-configLoader.prototype._hasYAMLConfig = function () {
+/**
+ * Check to see if a YAML config file exists.
+ *
+ * @returns {bool} True if a YAML config file exists, false if not.
+ */
+configLoader.prototype.hasYAMLConfig = function () {
 
   try {
     fs.statSync('holograph_config.yml');
@@ -41,7 +71,12 @@ configLoader.prototype._hasYAMLConfig = function () {
 
 };
 
-configLoader.prototype._hasJSConfig = function () {
+/**
+ * Check to see if a JavaScript config file exists.
+ *
+ * @returns {bool} True if a JavaScript config file exists, false if not.
+ */
+configLoader.prototype.hasJSConfig = function () {
 
   try {
     fs.statSync('holograph_config.js');
